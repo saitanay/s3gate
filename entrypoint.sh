c168c7e5f8e17a610a49be54104aa3a3c23dbd45
saitanay <saitanay@gmail.com>
2026-06-13 12:12:23 +0530

    Initial

---
+++ entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+set -e
+
+CONFIG_FILE="/config/config.json"
+
+HOST=$(jq -r '.storagebox.host' "$CONFIG_FILE")
+USERNAME=$(jq -r '.storagebox.username' "$CONFIG_FILE")
+PASSWORD=$(jq -r '.storagebox.password' "$CONFIG_FILE")
+PROTOCOL=$(jq -r '.storagebox.protocol' "$CONFIG_FILE")
+
+ACCESS_KEY=$(jq -r '.s3.access_key_id' "$CONFIG_FILE")
+SECRET_KEY=$(jq -r '.s3.secret_access_key' "$CONFIG_FILE")
+PORT=$(jq -r '.s3.listen_port' "$CONFIG_FILE")
+
+mkdir -p /root/.config/rclone
+
+cat > /root/.config/rclone/rclone.conf <<EOF
+[storagebox]
+type = webdav
+url = https://${HOST}
+vendor = other
+user = ${USERNAME}
+pass = $(rclone obscure "${PASSWORD}")
+EOF
+
+echo "Starting S3 gateway on port ${PORT}..."
+exec rclone serve s3 storagebox: \
+  --addr ":${PORT}" \
+  --auth-key "${ACCESS_KEY}" \
+  --auth-secret "${SECRET_KEY}" \
+  --vfs-cache-mode full
